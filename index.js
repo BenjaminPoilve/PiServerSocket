1ac3485361fe064bb3a56939620591b31b4737b7
Steven BOEHM <boehm_s@etna-alternance.net>
2018-05-23 17:44:11 +0200

    add debug on reconnectWebsocket and timeout 3000 ms

--- index.js
+++ index.js
@@ -4,40 +4,42 @@ const WS = require("ws");
 
 const options = {
     WebSocket: WS, // custom WebSocket constructor
+    debug: true,
+    reconnectInterval: 3000
 };
+
 init=false;
 
 var SerialPort = require('serialport');
 var port = new SerialPort('/dev/cu.usbserial-A5055PCP', {
-  baudRate: 250000
+    baudRate: 250000
 }, function (err) {
-  if (err) {
-    return console.log('Error: ', err.message);
-  }
+    if (err) {
+	return console.log('Error: ', err.message);
+    }
 });
 
 
 port.on('data', function (data) {
-  console.log('Data:', data.toString('utf8'));
-  if(data.toString('utf8').trim()=="ok" && init==false){
-      port.write("G91/n", function(err) {
-      if (err) {
-        return console.log('Error on write: ', err.message);
-      }
-      console.log('init done');
-      init=true;
-  })};
+    console.log('Data:', data.toString('utf8'));
+    if(data.toString('utf8').trim()=="ok" && init==false){
+	port.write("G91/n", function(err) {
+	    if (err) {
+		return console.log('Error on write: ', err.message);
+	    }
+	    console.log('init done');
+	    init=true;
+	})};
 });
 
 const rws = new ReconnectingWebSocket('ws://ec2-35-180-32-199.eu-west-3.compute.amazonaws.com:8080', [], options);
 
 rws.addEventListener('message', function (m) {
-  console.log(m.data)
-  port.write(m.data, function(err) {
-  if (err) {
-    return console.log('Error on write: ', err.message);
-  }
-  console.log('message written');
+    console.log(m.data)
+    port.write(m.data, function(err) {
+	if (err) {
+	    return console.log('Error on write: ', err.message);
+	}
+	console.log('message written');
+    });
 });
-});
-
