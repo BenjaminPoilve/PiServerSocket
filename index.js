82a1384a36cb9b7e7673bcfe50fab2b0e0764709
Steven BOEHM <boehm_s@etna-alternance.net>
2018-05-30 15:03:32 +0200

    debug rws

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
 #!/usr/bin/env node
-const ReconnectingWebSocket = require("reconnecting-websocket");
-const WS = require("ws");
 
-const options = {
+const RecoWS	= require("reconnecting-websocket");
+const WS	= require("ws");
+const SP	= require('serialport');
+
+const WS_URL	= 'ws://ec2-35-180-32-199.eu-west-3.compute.amazonaws.com:8080';
+const USB_PATH	= '/dev/cu.usbserial-A5055PCP';
+
+let init = false;
+
+const rwsOptions = {
     WebSocket: WS, // custom WebSocket constructor
     debug: true,
     reconnectInterval: 3000
 };
 
-init=false;
+const port = new SP(
+    USB_PATH,
+    { baudRate: 250000 },
+    err => err ? console.log('Error: ', err.message) : null
+);
 
-var SerialPort = require('serialport');
-var port = new SerialPort('/dev/cu.usbserial-A5055PCP', {
-    baudRate: 250000
-}, function (err) {
-    if (err) {
-	return console.log('Error: ', err.message);
+
+port.on('data', data => {
+    console.log('Data:', data.toString('utf8'));
+
+    if (data.toString('utf8').trim() == "ok" && init == false) {
+	port.write("G91/n", err => {
+	    if (err) {
+		console.log('Error on write: ', err.message);
+	    } else {
+		console.log('init done');
+		init = true;
+	    }
+	});
     }
 });
 
+const rws = new RecoWS(WS_URL, [], rwsOptions);
 
-port.on('data', function (data) {
-    console.log('Data:', data.toString('utf8'));
-    if(data.toString('utf8').trim()=="ok" && init==false){
-	port.write("G91/n", function(err) {
-	    if (err) {
-		return console.log('Error on write: ', err.message);
-	    }
-	    console.log('init done');
-	    init=true;
-	})};
+rws.addEventListener('message', msg => {
+    console.log(msg.data);
+    port.write(msg.data, err =>  console.log(err ? `Error on write: ${err.message}` : 'message written'));
 });
 
-const rws = new ReconnectingWebSocket('ws://ec2-35-180-32-199.eu-west-3.compute.amazonaws.com:8080', [], options);
-
-rws.addEventListener('message', function (m) {
-    console.log(m.data)
-    port.write(m.data, function(err) {
-	if (err) {
-	    return console.log('Error on write: ', err.message);
-	}
-	console.log('message written');
-    });
+rws.addEventListener('close', msg => {
+    console.log('CLOSE --- IT SHOULD NOT CLOSE !!!');
+    console.log(msg);
 });
